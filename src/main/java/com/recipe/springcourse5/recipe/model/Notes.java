df62e2cbe4597e0f86452657f38f3daa36d00f7e
Anton Mösenbacher <anton.mosenbacher@kentor.se>
2018-03-23 17:06:20 +0100

    Added entitys to database

---
+++ src/main/java/com/recipe/springcourse5/recipe/model/Notes.java
@@ -0,0 +1,20 @@
+package com.recipe.springcourse5.recipe.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private @Getter @Setter Long id;
+
+    @OneToOne
+    private @Getter @Setter Recipe recipe;
+    @Lob
+    private @Getter @Setter String recipeNotes;
+
+}
--- src/main/java/com/recipe/springcourse5/recipe/model/Recipe.java
+++ src/main/java/com/recipe/springcourse5/recipe/model/Recipe.java
@@ -1,4 +1,29 @@
 package com.recipe.springcourse5.recipe.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
 public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private @Getter @Setter Long id;
+
+    private @Getter @Setter String description;
+    private @Getter @Setter Integer prepTime;
+    private @Getter @Setter Integer cookTime;
+    private @Getter @Setter Integer servings;
+    private @Getter @Setter String source;
+    private @Getter @Setter String url;
+    private @Getter @Setter String directions;
+
+    @Lob
+    private @Getter @Setter Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private @Getter @Setter Notes notes;
+
 }
