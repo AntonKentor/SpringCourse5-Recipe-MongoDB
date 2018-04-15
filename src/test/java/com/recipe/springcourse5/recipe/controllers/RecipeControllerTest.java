ebf2ffd12503c2f5553480db450d6982036bcd43
amosenbacher <anton.mosenbacher@kentor.se>
2018-04-15 19:00:32 +0200

    Displaying some extra atributes. Made some change to test.

--- src/test/java/com/recipe/springcourse5/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/recipe/springcourse5/recipe/controllers/RecipeControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -35,6 +37,7 @@ public class RecipeControllerTest {
         recipe.setId(1L);
 
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"));
