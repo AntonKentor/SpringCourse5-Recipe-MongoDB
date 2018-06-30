c127087a58f17580b291a6948d39cea5f829d123
amosenbacher <anton.mosenbacher@kentor.se>
2018-06-30 17:30:14 +0200

    changed propertyvalue for liquibase

--- database/src/test/java/com/recipe/springcourse5/database/DatabaseApplicationTest.java
+++ database/src/test/java/com/recipe/springcourse5/database/DatabaseApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("prod")
+@ActiveProfiles("dev")
 public class DatabaseApplicationTest {
 
     @Test
