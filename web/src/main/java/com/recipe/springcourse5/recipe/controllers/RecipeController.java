6d047106fd188e2d0b937235b25bbf90b731b342
amosenbacher <anton.mosenbacher@kentor.se>
2018-07-05 09:44:30 +0200

    Refactoring.

--- web/src/main/java/com/recipe/springcourse5/recipe/controllers/RecipeController.java
+++ web/src/main/java/com/recipe/springcourse5/recipe/controllers/RecipeController.java
@@ -3,6 +3,7 @@ package com.recipe.springcourse5.recipe.controllers;
 import com.recipe.springcourse5.recipe.commands.RecipeCommand;
 import com.recipe.springcourse5.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.asn1.x500.style.RFC4519Style;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,28 +24,28 @@ public class RecipeController {
 
     @GetMapping("/{id}/show")
     public String showById(@PathVariable String id, Model model) {
-        log.info("In method showById with id : " + id);
+        log.info("Showing recipe with id : " + id);
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
     @GetMapping("/new")
     public String newRecipe(Model model) {
-        log.info("In method newRecipe");
+        log.info("add new recipe form");
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeForm";
     }
 
     @GetMapping("/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
-        log.info("updating recipe with id: +" + id);
+        log.info("updating recipe with id: " + id);
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeForm";
     }
 
     @PostMapping("/saveOrUpdate")
     public String saveOrUpdateRecipe(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
-        log.info("Saving/updating recipe");
+        log.info("Saving/updating recipe "+command.getId());
 
         if (bindingResult.hasErrors()) {
             bindingResult.getAllErrors().forEach(objectError -> {
